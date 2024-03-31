23ab2c0bd54e147805549f622b61bc8dd93a9916
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2024-04-01 01:55:29 +0600

    update type o mistake in read quran section Qura to Quran

--- src/components/Home/ReadQuran.jsx
+++ src/components/Home/ReadQuran.jsx
@@ -15,7 +15,7 @@ const ReadQuran = () => {
 
     return (
         <div className='px-4 md:px-20 my-16'>
-            <h1 className='text-[26px] md:text-[42px] text-center font-bold text-primary leading-[52px]'>Read holy Qura with<br></br>
+            <h1 className='text-[26px] md:text-[42px] text-center font-bold text-primary leading-[52px]'>Read holy Quran with<br></br>
             translations</h1>
             <QuranFilter isActive={isActive} setIsActive={setIsActive}></QuranFilter>
             {
