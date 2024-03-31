4d6bd3fe167d4e036fca6b6086afa679561fd019
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2024-03-31 20:10:43 +0600

    surah and juzz button responsive

--- src/components/Home/QuranFilter.jsx
+++ src/components/Home/QuranFilter.jsx
@@ -8,8 +8,8 @@ const QuranFilter = () => {
     return (
         <div className='flex justify-between items-center'>
             <div className="border-base-200 border-2 rounded-lg p-1 flex gap-2">
-                <button id='btn' onClick={()=>{setIsActive(1)}} className={isActive === 1 ? 'btn btn-primary' : 'btn btn-ghost'}>Surah</button>
-                <button id='btn' onClick={()=>{setIsActive(2)}} className={isActive === 2 ? 'btn btn-primary' : 'btn btn-ghost'}>Juzz</button>
+                <button onClick={()=>{setIsActive(1)}} className={isActive === 1 ? 'btn btn-sm md:btn-md btn-primary' : 'btn btn-sm md:btn-md btn-ghost'}>Surah</button>
+                <button onClick={()=>{setIsActive(2)}} className={isActive === 2 ? 'btn btn-sm md:btn-md btn-primary' : 'btn btn-sm md:btn-md btn-ghost'}>Juzz</button>
             </div>
             <div className="">
                 <label className='text-info'>Sort by</label>
