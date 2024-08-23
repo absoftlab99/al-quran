6c5b04b4589b1f538356ae5e07a2d3740fc7c7d9
absoftlab99 <abdullahtech2@outlook.com>
2024-08-24 00:20:28 +0600

    sticky header added

--- src/components/Shared/Header.jsx
+++ src/components/Shared/Header.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import PhoneProfile from './PhoneProfile';
 const Header = () => {
     return (
-        <div className="">
+        <div className="sticky top-0 z-50">
             <div className="navbar bg-base-100">
                 <div className="navbar-start">
                     <Image
