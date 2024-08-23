cdc94ae3950590ae96c70578b28a00b0f4758a51
absoftlab99 <abdullahtech2@outlook.com>
2024-08-24 00:16:17 +0600

    api link changed

--- src/app/[id]/[name]/page.js
+++ src/app/[id]/[name]/page.js
@@ -2,8 +2,8 @@ import Surah from '@/components/Pages/Surah';
 import React from 'react';
 
 const getSurahBySurahNumber= async(surahId)=>{
-    // const data = await fetch('https://api.alquran.cloud/v1/quran/ar.alafasy')
-    const data = await fetch("http://localhost:3000/alafasy.json")
+    const data = await fetch('https://api.alquran.cloud/v1/quran/ar.alafasy')
+    // const data = await fetch("http://localhost:3000/alafasy.json")
     const allSurahs = await data.json()
 
     const surah = allSurahs.data.surahs.find(item =>item.number === surahId)
